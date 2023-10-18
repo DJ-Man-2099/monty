de1de6c684fe940366ce5bfd4fe8834e74ff44ce
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-19 01:00:28 +0300

    Task 1

--- opcodes.c
+++ opcodes.c
@@ -12,10 +12,8 @@ void push(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
 	if ((*stack) != NULL)
-	{
 		(*stack)->prev = global_current_stack;
-		global_current_stack->next = (*stack);
-	}
+	global_current_stack->next = (*stack);
 	(*stack) = global_current_stack;
 }
 /**
