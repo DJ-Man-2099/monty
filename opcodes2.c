8a4c534456a00de9c88957c58988f70ee47fe155
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-19 21:25:21 +0300

    Optional Task 2

--- opcodes2.c
+++ opcodes2.c
@@ -48,6 +48,35 @@ void sub(stack_t **stack, unsigned int line_number)
 	free(old_head);
 }
 /**
+ * div - monty opcode
+ * @stack: stack to update
+ * @line_number: number of instruction line
+ *
+ * executes the div
+ *
+ * Return: void
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	int sum = 0;
+	stack_t *old_head = *stack;
+
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	sum = (*stack)->next->n / (*stack)->n;
+	(*stack) = (*stack)->next;
+	(*stack)->n = sum;
+	free(old_head);
+}
+/**
  * nop - monty opcode
  * @stack: stack to update
  * @line_number: number of instruction line
